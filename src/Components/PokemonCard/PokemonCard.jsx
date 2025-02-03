faff0e96d30b8b02bd2bd57fac6a40fd6cd82a69
Yash Srivastava <120123563+yashsrivasta7a@users.noreply.github.com>
2025-02-04 01:13:37 +0530

    Card Updated

--- src/Components/PokemonCard/PokemonCard.jsx
+++ src/Components/PokemonCard/PokemonCard.jsx
@@ -3,6 +3,7 @@ import './PokemonCard.css';
 
 function PokemonCard({ pokemonData }) {
   const primaryType = pokemonData.types[0].type.name;
+
   const getColor = (type) => {
     switch (type) {
       case 'fire':
@@ -20,21 +21,32 @@ function PokemonCard({ pokemonData }) {
       case 'ghost':
         return 'linear-gradient(45deg, #6f42c1, #8a2be2)';
       default:
-        return 'linear-gradient(45deg, #ccc 50% , grey )';
+        return 'linear-gradient(45deg, #ccc, grey)';
     }
   };
-  
 
-  const cardColor = getColor(primaryType); 
+  const cardColor = getColor(primaryType);
 
   return (
     <section className='pokemon-card' style={{ backgroundImage: cardColor }}>
-      <img src={pokemonData.sprites.front_default} alt={pokemonData.name} className='pokemon-image' />
-      <div className='pokemon-name'>
+      <img
+        src={pokemonData.sprites.front_default}
+        alt={pokemonData.name}
+        className='pokemon-image'
+      />
+      <h1 className='pokemon-name'>
         {pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1)}
+      </h1>
+      <div className='pokemon-type'>
+        {pokemonData.types.map((type) => type.type.name).join(', ')}
       </div>
-      <div className='pokemon-details'>
-        <p>Type: {pokemonData.types.map(type => type.type.name).join(', ')}</p>
+      <div className='pokemon-stats'>
+        <p><strong>Height:</strong> {pokemonData.height}</p>
+        <p><strong>Weight:</strong> {pokemonData.weight}</p>
+        <p><strong>Speed:</strong> {pokemonData.stats[5].base_stat}</p>
+        <p><strong>Experience:</strong> {pokemonData.base_experience}</p>
+        <p><strong>Attack:</strong> {pokemonData.stats[1].base_stat}</p>
+        <p><strong>Abilities:</strong> {pokemonData.abilities.map((ability) => ability.ability.name).join(', ')}</p>
       </div>
     </section>
   );
